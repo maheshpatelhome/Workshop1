77e56c588b07496d992fb077935da69cfff5cd57
Mahesh Patel <mahesh.patel.home@gmail.com>
2020-10-15 14:06:54 +0100

    added power operator

--- CalculatorStep1.py
+++ CalculatorStep1.py
@@ -1,4 +1,4 @@
-operator = input("Please Enter operator (X, +, -, /) ")
+operator = input("Please Enter operator (X, +, -, /, ^) ")
 int1 = int(input("Please enter integer 1 "))
 int2 = int(input("Please enter integer 2 "))
 
@@ -10,3 +10,5 @@ elif operator == "-":
     print (int1 - int2)
 elif operator == "/":
     print (int1 / int2)
+elif operator =="^":
+    print (pow(int1, int2))
